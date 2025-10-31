85bb30dfb7723a8eb59b919fd64940734df78d3a
冯汇宝 <fenghuibao@fenghuibaodeMacBook-Pro.local>
2025-10-30 21:02:24 -0700

    v0.0.2

--- src/segment_any_confocal_images/__init__.py
+++ src/segment_any_confocal_images/__init__.py
@@ -1,21 +1,5 @@
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
+__version__ = "0.0.0"
 
+from ._widget import SegmentConfocalWidget  # noqa: F401
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
-from ._writer import write_multiple, write_single_image
-
-__all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
-)
+__all__ = ["SegmentConfocalWidget"]
